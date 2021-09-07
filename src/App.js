2a56315744ab3214d451cbeb9c14782af758b39e
fatma-baba <haniyamaammer17@gmail.com>
2021-09-07 23:40:23 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
+import './style.css'
+import srcImage from './imageInSrc.jpg'
 function App() {
+  const name = "Fatma Maammer Saadallah"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div style={{ border:'solid 1px black', maxWidth:'100vw'}}>
+        <h1 className={"title red"}>{name}</h1>
+        <br />
+        <img src={srcImage}/>
+        <br />
+        <img src="/imageInPublic.jpeg"/>
+      </div>
+      <video width="320" height="240" controls>
+      <source src="/myVideo.mp4" type="video/mp4"/>
+      </video>
+    </React.Fragment>
   );
 }
 
